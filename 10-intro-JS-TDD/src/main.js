45a74e3b12d41542ba944e7626f54b5af5769167
mberkay09 <berkaybayhan@hotmail.com>
2018-11-14 18:47:51 +0100

    changes from last session

--- 10-intro-JS-TDD/src/main.js
+++ 10-intro-JS-TDD/src/main.js
@@ -1,25 +1,19 @@
-module.exports = add;
-
-function add(a, b) {
-    /*
-
-      Write some code here !!!
-
-     */
-    return null; // This can be deleted as well ;)
-}
-
+const add = require('../src/main');
 
 /*
-   Utilities functions (just for helping)
+   TODO: Make me pass! (step 1)
  */
+test('should return 0 when empty string provided', () => {
+    expect(add('')).toBe(0);
+});
 
-// OBS: this is quite a ugly helper method: DELETE ME!
-function isUndefined(x) {
-    return x === undefined;
-}
 
-// Tips: DON'T use Number.NaN
-function isANumber(x) {
-    return typeof(x) === 'number';
-}
+test('should return sum when to integers are provided', () => {
+    expect(add(4, 5)).toBe(9);
+});
+
+
+
+test('should return the value of the only numeric parameter when the other parameter is not a number', () => {
+    expect(add("yo!", 6)).toBe(6);
+});
